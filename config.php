09d9ec7a8b4229dc09504c9df0828a2c693ead52
winne27 <werner@schaeffer.net>
2017-06-03 18:52:49 +0200

    Version 1.1

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $url[0] = 'http://192.168.0.14';
 
 // for further devices copy this lines above and change index from 0 to i (1,2,3, ...)  and change $dnr[i] (normally to i)
 
-// connect data to MySql database
+// connection data to MySql database
 $host = 'localhost';
 $port = '3601'; // 3601 is default port for MySql and MariaDb databases
 $dbname = 'youless';
